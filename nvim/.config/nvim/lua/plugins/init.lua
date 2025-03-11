3a5c4fed10d62c1f946ff0a1137e685e48dd6e7e
Nilesh Ugale <nilesh.r.ugale@gmail.com>
2025-03-11 21:17:46 +0530

    fix: Syntax highlight in neorg inline code
    - Not sure why it fixed the issue

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -93,7 +93,22 @@ return {
     },
     {
         "catppuccin/nvim",
-        name = "catppuccin"
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                flavour = "mocha", -- latte, frappe, macchiato, mocha
+                background = {
+                    -- :h background
+                    light = "latte",
+                    dark = "mocha",
+                },
+                transparent_background = true
+            })
+            vim.cmd.colorscheme('catppuccin')
+            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        end,
     },
     { "nvim-treesitter/playground" },
     { "theprimeagen/harpoon" },
