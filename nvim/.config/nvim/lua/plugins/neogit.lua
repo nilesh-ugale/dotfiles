d3d78238c73105b5bfab878d1a83abafce774081
Nilesh Ugale <nilesh.ugale@sedemac.com>
2025-04-17 09:51:05 +0000

    feat: Add gitsigns plugin

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -64,5 +64,66 @@ return {
                 },
             })
         end
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require('gitsigns').setup()
+            local gitsigns = require('gitsigns')
+
+            -- Navigation
+            vim.keymap.set('n', ']c', function()
+                if vim.wo.diff then
+                    vim.cmd.normal({ ']c', bang = true })
+                else
+                    gitsigns.nav_hunk('next')
+                end
+            end)
+
+            vim.keymap.set('n', '[c', function()
+                if vim.wo.diff then
+                    vim.cmd.normal({ '[c', bang = true })
+                else
+                    gitsigns.nav_hunk('prev')
+                end
+            end)
+
+            -- Actions
+            vim.keymap.set('n', '<leader>hs', gitsigns.stage_hunk)
+            vim.keymap.set('n', '<leader>hr', gitsigns.reset_hunk)
+
+            vim.keymap.set('v', '<leader>hs', function()
+                gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+            end)
+
+            vim.keymap.set('v', '<leader>hr', function()
+                gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+            end)
+
+            vim.keymap.set('n', '<leader>hS', gitsigns.stage_buffer)
+            vim.keymap.set('n', '<leader>hR', gitsigns.reset_buffer)
+            vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk)
+            vim.keymap.set('n', '<leader>hi', gitsigns.preview_hunk_inline)
+
+            vim.keymap.set('n', '<leader>hb', function()
+                gitsigns.blame_line({ full = true })
+            end)
+
+            vim.keymap.set('n', '<leader>hd', gitsigns.diffthis)
+
+            vim.keymap.set('n', '<leader>hD', function()
+                gitsigns.diffthis('~')
+            end)
+
+            vim.keymap.set('n', '<leader>hQ', function() gitsigns.setqflist('all') end)
+            vim.keymap.set('n', '<leader>hq', gitsigns.setqflist)
+
+            -- Toggles
+            vim.keymap.set('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+            vim.keymap.set('n', '<leader>tw', gitsigns.toggle_word_diff)
+
+            -- Text object
+            vim.keymap.set({ 'o', 'x' }, 'ih', gitsigns.select_hunk)
+        end,
     }
 }
