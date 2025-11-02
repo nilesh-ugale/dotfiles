e1c69b372e58a752a8bb0d53cbd95f0589d7f24b
Nilesh Ugale <nilesh.r.ugale@gmail.com>
2025-11-02 18:15:37 +0530

    Added kitty config

--- scripts/.scripts/clr_scr.zsh
+++ scripts/.scripts/clr_scr.zsh
@@ -1,8 +1,17 @@
 #!/bin/zsh
 var="true"
-clear;
-if [ -n $PAB ]
-then
+
+# Detect if terminal is Kitty
+if [[ "$TERM" == "xterm-kitty" ]]; then
+    # Clear using Kitty-specific escape sequences
+    printf '\033[2J\033[H\033[3J'
+else
+    clear
+fi
+
+# Check PAB variable
+if [[ -n "$PAB" ]]; then
     [[ "$PAB" == "$var" ]] && print ${(pl:$LINES::\n:):-}
 fi
 exit 0
+
