c354a99ff5365a0e83300749bc9153fbbea0d651
Nilesh Ugale <nilesh.r.ugale@gmail.com>
2025-12-27 19:46:36 +0530

    Sat Dec 27 07:46:36 PM IST 2025

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -4,10 +4,35 @@ return {
         build = function()
             local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
             ts_update()
-        end
-    },
-    {
-        'nvim-treesitter/playground'
+        end,
+        opts = {
+            modules  = {},
+            -- A list of parser names, or "all"
+            ensure_installed = {
+                "vimdoc",
+                "c",
+                "lua",
+                "markdown_inline",
+            },
+            ignore_install = {
+                'org',
+            },
+            -- Install parsers synchronously (only applied to `ensure_installed`)
+            sync_install = false,
+
+            -- Automatically install missing parsers when entering buffer
+            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+            auto_install = true,
+
+            highlight = {
+                -- `false` will disable the whole extension
+                enable = true,
+                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                -- Instead of true it can also be a list of languages
+            },
+        },
     },
     {
         'nvim-treesitter/nvim-treesitter-context'
