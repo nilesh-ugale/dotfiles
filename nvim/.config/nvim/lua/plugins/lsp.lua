22012d172bceefdd5a91cd011963ed45c5eb2334
Nilesh Ugale <nilesh.r.ugale@gmail.com>
2026-02-04 16:11:41 +0530

    Wed Feb  4 04:11:41 PM IST 2026

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -12,4 +12,14 @@ return {
             },
         },
     },
+    {
+        'nvimdev/lspsaga.nvim',
+        config = function()
+            require('lspsaga').setup({})
+        end,
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter', -- optional
+            'nvim-tree/nvim-web-devicons',     -- optional
+        }
+    }
 }
