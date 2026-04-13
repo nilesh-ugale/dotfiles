a98754e7316e9cea97e783435c8e9e522c363828
Nilesh Ugale <nilesh.r.ugale@gmail.com>
2026-04-13 17:24:22 +0530

    Mon Apr 13 05:24:22 PM IST 2026

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -69,15 +69,18 @@ vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(event)
         local opts = { buffer = event.buf, remap = false }
 
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover({ border = 'rounded' }) end, opts)
+        vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<cr>", opts)
+        vim.keymap.set("n", "gp",  "<cmd>Lspsaga peek_definition<cr>", opts)
+        vim.keymap.set("n", "gt",  "<cmd>Lspsaga peek_type_definition<cr>", opts)
+        vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<cr>", opts)
         vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
         vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = 1 }) end, opts)
         vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = -1 }) end, opts)
-        vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<cr>", opts)
+        vim.keymap.set("n", "<leader>ro", "<cmd>Lspsaga outline<cr>", opts)
+        vim.keymap.set("n", "<leader>rr", "<cmd>Lspsaga finder<cr>", opts)
+        vim.keymap.set("n", "<leader>rn",  "<cmd>Lspsaga rename<cr>", opts)
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
     end
 })
